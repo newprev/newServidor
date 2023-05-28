eeb39e07fcfcac307ec7a117282f3abc852084eb
Israel Alves <israel.usp@gmail.com>
2023-05-28 17:01:56 -0300

    Adicionando CORS ao django

--- backend/newPrevServ/settings.py
+++ backend/newPrevServ/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 
     # Meus apps
     'apps.advogado',
@@ -80,6 +81,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'newPrevServ.urls'
