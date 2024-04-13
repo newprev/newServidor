180e471443e39b3959dc213c588aecbaeaf631da
Israel Alves <israel.usp@gmail.com>
2024-04-13 10:47:36 -0300

    Atualização Makefile e usuarioSQL.sql

--- backend/instalacao/usuarioSQL.sql
+++ backend/instalacao/usuarioSQL.sql
@@ -13,6 +13,7 @@ CREATE PROCEDURE concedePermissoes ()
 	BEGIN
 		/*Caso os usuários já existam*/
 		DROP USER IF EXISTS 'NEWPREV'@'localhost';
+		DROP USER IF EXISTS 'NEWPREV'@'127.0.0.1';
 		DROP USER IF EXISTS 'NEWPREV'@'%.%.%.%';
 		DROP USER IF EXISTS 'NEWPREV'@'%';
 		DROP USER IF EXISTS 'NEWPREV'@'0';
@@ -20,6 +21,7 @@ CREATE PROCEDURE concedePermissoes ()
 		
 		/* Criação do usuário */
 		CREATE USER 'NEWPREV'@'localhost' IDENTIFIED WITH mysql_native_password BY '__NewPrev2024__';
+		CREATE USER 'NEWPREV'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY '__NewPrev2024__';
 		CREATE USER 'NEWPREV'@'%.%.%.%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2024__';
 		CREATE USER 'NEWPREV'@'%' IDENTIFIED WITH mysql_native_password BY '__NewPrev2024__';
 		CREATE USER 'NEWPREV'@'0' IDENTIFIED WITH mysql_native_password BY '__NewPrev2024__';
@@ -27,6 +29,7 @@ CREATE PROCEDURE concedePermissoes ()
 	
 		/* Concedendo privilégios */
 		GRANT ALL PRIVILEGES ON *.* TO 'NEWPREV'@'localhost';
+		GRANT ALL PRIVILEGES ON *.* TO 'NEWPREV'@'127.0.0.1';
 		GRANT SELECT, UPDATE, INSERT, DELETE, INDEX, CREATE, REFERENCES, ALTER ON *.* TO 'NEWPREV'@'%.%.%.%';
 		GRANT SELECT, UPDATE, INSERT, DELETE, INDEX, CREATE, REFERENCES, ALTER ON *.* TO 'NEWPREV'@'%';
 		GRANT SELECT, UPDATE, INSERT, DELETE, INDEX, CREATE, REFERENCES, ALTER ON *.* TO 'NEWPREV'@'0';
